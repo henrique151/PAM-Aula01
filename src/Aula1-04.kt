1c2978671287ceb26e800800d1bcd790f00a3446
unknown <henrique.sousa60@etec.sp.gov.br>
2024-02-20 20:44:26 -0300

    Aula de PAM sobre os conceitos básico de Kotlin 20-02-24

---
+++ src/Aula1-04.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    imprimeirSoma(4, 5)
+ }
+ 
+ fun imprimeirSoma(a: Int, b:Int) {
+     println(a+b)
+ }
+ 
---
+++ src/Aula1-10.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    val nota: Double = 6.9
+     if(nota >= 7) {
+         println("Aprovado!!")
+     } else {
+         println("Reprovado!!")
+     }
+  }
+ 
+ 
